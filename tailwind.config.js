50d5361fb9d2163fb33a5b0262158895a139a29b
Joel Adelugba <adelugba.joel@gmail.com>
2024-08-04 22:45:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config}*/
+module.exports = {
+  content: [
+    './src/LOADER/*.html',
+    './src/LANDING/*.html',
+    './src/GOLDMAP/*.html',
+    './src/GALLERY/*.html',
+    './src/MINT/*.html',
+    './src/FOOTER/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
